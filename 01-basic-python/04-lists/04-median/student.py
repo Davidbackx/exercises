f0e2f7b502e546107286b02fd44186fe49059e18
David Backx <david.backx@student.ucll.be>
2021-02-17 11:23:18 +0100

    scripting

--- 01-basic-python/04-lists/04-median/student.py
+++ 01-basic-python/04-lists/04-median/student.py
@@ -1,1 +1,10 @@
 # Write your code here
+def median(ns):
+    xs = list
+    xs = sorted(ns)
+    length = len(xs)
+    if length % 2 == 0:
+        return (xs[length//2] + xs[length // 2 -1])/2       
+    else:
+        return xs[length//2]
+
--- 01-basic-python/05-strings/01-format-time/student.py
+++ 01-basic-python/05-strings/01-format-time/student.py
@@ -1,1 +1,9 @@
-# Write your code here
+# Write your code hereµ
+def format_time(h,m,s):
+    if h < 10:
+        str('0'+h)
+    if m < 10:
+        str('0'+m)
+    if s < 10:
+        str('0'+s)
+    return f"{h}:{m}:{s}"
